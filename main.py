2d3c0aa701cc14efa108314e173c426fc67e8b99
Anakha Ganesh <anakhag07@gmail.com>
2026-03-09 15:29:07 -0400

    change to experiment specs for custom

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ from experiments.reporters import (
 )
 from experiments.run import run_experiment
 
-RUN_CONFIGS = ["planted_logistic", "custom"]
-# RUN_CONFIGS = ["custom"]
+# RUN_CONFIGS = ["planted_logistic", "custom"]
+RUN_CONFIGS = ["custom"]
 
 def main() -> None:
     for config_name in RUN_CONFIGS:
